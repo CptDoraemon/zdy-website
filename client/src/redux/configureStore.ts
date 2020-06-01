c51b3992c9d25183fa140d95e286fc245cff4779
Xiaoxi <yxx0827@gmail.com>
2020-06-01 11:43:28 -0400

    try reduce bundle size

--- client/src/redux/configureStore.ts
+++ client/src/redux/configureStore.ts
@@ -3,13 +3,14 @@ import thunkMiddleware from 'redux-thunk'
 import state from "./state";
 import rootReducers from "./reducers/root-reducers";
 
-import { createLogger } from 'redux-logger'
-const loggerMiddleware = createLogger();
+// import { createLogger } from 'redux-logger'
+// const loggerMiddleware = createLogger();
 
 export default function configureStore() {
     return createStore(
         rootReducers,
         state,
-        process.env.REACT_APP_DEBUG === 'true' ? applyMiddleware(thunkMiddleware, loggerMiddleware) : applyMiddleware(thunkMiddleware),
+        // applyMiddleware(thunkMiddleware, loggerMiddleware),
+        applyMiddleware(thunkMiddleware),
     )
 }
